f541189751cfa0ae7894a4f9471a63fa4af09f5e
Enrico Rossini <enrico.rossini.uk@live.com>
2020-06-30 21:54:17 +0100

    Upload console application

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApplication1;
+using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Performing some task... ");
+            using (var progress = new ProgressBar())
+            {
+                for (int i = 0; i <= 1000; i++)
+                {
+                    progress.Report((double)i / 100);
+                    Thread.Sleep(20);
+                }
+            }
+            Console.WriteLine("Done.");
+        }
+    }
+}
